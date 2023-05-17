8981399d67eeab5af4c3e6fd762f57934b31c6d2
Salazar106 <jdps106@hotmail.com>
2023-05-17 17:05:53 -0500

    Se realiza consulta en db para trar la informacion de la tabla y se controla su presentacion con useState

--- src/components/FormularioMetaAsesor.jsx
+++ src/components/FormularioMetaAsesor.jsx
@@ -1,10 +1,13 @@
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import React from "react";
 import { useState } from 'react';
 import "./FormularioMetaAsesor.css";
 import Button from "../pure/Button";
+import axios from "axios";
+import Swal from "sweetalert2";
 
-const FormMetaAsesor = () => {
+
+const FormMetaAsesor = ({setrefreshTable}) => {
   const [antiguedad, setAntiguedad] = useState(null);
   const date = new Date();
   const month = date.getMonth() + 1
@@ -13,9 +16,25 @@ const FormMetaAsesor = () => {
   const periodos = date.getFullYear() + '-' + fullMonth
   const mayores = '>60'
   const menores = '<=60'
+
+  
+
   function handleChange(event) {
     setAntiguedad(event.target.value);
   }
+  
+  const alertSend = (title, icon, iconColor) => {
+    Swal.fire({
+      position: "center",
+      iconColor: iconColor,
+      icon: icon,
+      title: title,
+      showConfirmButton: false,
+      confirmButtonText: "",
+      confirmButtonColor: "",
+      timer: 3500,
+    });
+  };
 
 
 
@@ -29,11 +48,9 @@ const FormMetaAsesor = () => {
           <option value={menores}>{menores}</option>
         </select>
       </div>
-     
-      
+         
       <Formik
         initialValues={{
-          antiguedad: antiguedad,
           periodo: periodos,
           meta_bit: "",
           meta_bit_s1: "",
@@ -49,116 +66,157 @@ const FormMetaAsesor = () => {
           meta_dia_s5: "",
         }}
 
-
         validate={(values) => {
           const errors = {};
-          if (!values.antiguedad) {
-            errors.antiguedad = "Antiguedad is required";
-          }
-
+          
           if (!values.periodo) {
-            errors.periodo = "Periodo is required";
+            errors.periodo = "Periodo es requerido";
           }
           if (!values.meta_bit) {
-            errors.meta_bit = "meta_bit is required";
+            errors.meta_bit = "meta_bit es requerido";
           }
           if (!values.meta_bit_s1) {
-            errors.meta_bit_s1 = "meta_bit_s1 is required";
+            errors.meta_bit_s1 = "meta_bit_s1 es requerido";
           }
           if (!values.meta_bit_s2) {
             errors.meta_bit_s2 = "meta_bit_s2 isrequired";
           }
           if (!values.meta_bit_s3) {
-            errors.meta_bit_s3 = "meta_bit_s3 is required";
+            errors.meta_bit_s3 = "meta_bit_s3 es requerido";
           }
           if (!values.meta_bit_s4) {
-            errors.meta_bit_s4 = "meta_bit_s4 is required";
+            errors.meta_bit_s4 = "meta_bit_s4 es requerido";
           }
           if (!values.meta_bit_s5) {
-            errors.meta_bit_s5 = "meta_bit_s5 is required";
+            errors.meta_bit_s5 = "meta_bit_s5 es requerido";
           }
           if (!values.meta_dia) {
-            errors.meta_dia = "meta_dia is required";
+            errors.meta_dia = "meta_dia es requerido";
           }
           if (!values.meta_dia_s1) {
-            errors.meta_dia_s1 = "meta_dia_s1 is required";
+            errors.meta_dia_s1 = "meta_dia_s1 es requerido";
           }
           if (!values.meta_dia_s2) {
-            errors.meta_dia_s2 = "meta_dia_s2 is required";
+            errors.meta_dia_s2 = "meta_dia_s2 es requerido";
           }
           if (!values.meta_dia_s3) {
-            errors.meta_dia_s3 = "meta_dia_s3 is required";
+            errors.meta_dia_s3 = "meta_dia_s3 es requerido";
           }
           if (!values.meta_dia_s4) {
-            errors.meta_dia_s4 = "meta_dia_s4 is required";
+            errors.meta_dia_s4 = "meta_dia_s4 es requerido";
           }
           if (!values.meta_dia_s5) {
-            errors.meta_dia_s5 = "meta_dia_s5 is required";
+            errors.meta_dia_s5 = "meta_dia_s5 es requerido";
           }
-
           return errors;
+        }}
+
+        onSubmit={(values, { resetForm }) => {
+          if(antiguedad===null){
+            alertSend("Por favor seleccione la antiguedad","warning")
+            return
+          }
+            const config = { 
+            headers: { 'content-type': 'multipart/form-data' }
+            }
+            let newValue = {"antiguedad":antiguedad}
+            values=Object.assign(values, newValue)
+             axios.post('https://api.amstigo.com.co/mto/mestasAsesor/update.php', values,config).then(( res ) => {
+              setrefreshTable(null)
+              if(res.data===1){
+                alertSend('Se actualizaron las metas del asesor con éxito', 'success','#002a8cbe' )
+              }else{
+                alertSend('Ocurrio un error, vuelve a intentarlo', 'error')
+              }
+
+              setTimeout(() => {
+                setrefreshTable(true)
+              }, 250);
+            })
+
+            
+          resetForm();
+         
+        
         }}
       >
         <Form className="formulario">
             <label className="label-fecha">Periodo: {periodos}</label>
-            <Field className="input-fecha" type="text" name="periodo" disabled/>
             <div className="tittle">
                 <h1>Antiguedad : {antiguedad || "---"}</h1>
             </div>
             
           <div className="container">
+          <h2 className="metas-tittle">Metas Bitacora</h2>
             <div className="metas">
                 <div className="form-Group">
                     <label className="label">meta_bit:</label>
-                    <Field className="input" type="number" name="" />
+                    <Field className="input" type="number" name="meta_bit" />
+                    <ErrorMessage name="meta_bit"/>
                 </div>
                 <div className="form-Group">
                     <label className="label">meta_bit_s1:</label>
-                    <Field className="input" type="number" name="" />
+                    <Field className="input" type="number" name="meta_bit_s1" />
+                    <ErrorMessage name="meta_bit_s1"/>
                 </div>
                 <div className="form-Group">
                     <label className="label">meta_bit_s2:</label>
-                    <Field className="input" type="number" name="" />
+                    <Field className="input" type="number" name="meta_bit_s2" />
+                    <ErrorMessage name="meta_bit_s2"/>
                 </div>
                 <div className="form-Group">
                     <label className="label">meta_bit_s3:</label>
-                    <Field className="input" type="number" name="" />
+                    <Field className="input" type="number" name="meta_bit_s3" />
+                    <ErrorMessage name="meta_bit_s3"/>
                 </div>
                 <div className="form-Group">
                     <label className="label">meta_bit_s4:</label>
-                    <Field className="input" type="number" name="" />
+                    <Field className="input" type="number" name="meta_bit_s4" />
+                    <ErrorMessage name="meta_bit_s4"/>
                 </div>
                 <div className="form-Group">
                     <label className="label">meta_bit_s5:</label>
-                    <Field className="input" type="number" name="" />
+                    <Field className="input" type="number" name="meta_bit_s5" />
+                    <ErrorMessage name="meta_bit_s5"/>
+
                 </div>
             </div>
+            <h2 className="metas-tittle">Metas Dias</h2>
             <div className="metas">
                 <div className="form-Group">
                     <label className="label">meta_dia:</label>
-                    <Field className="input" type="number" name="" />
+                    <Field className="input" type="number" name="meta_dia" />
+                    <ErrorMessage name="meta_dia"/>
+
                 </div>
                 <div className="form-Group">
                     <label className="label">meta_dia_s1:</label>
-                    <Field className="input" type="number" name="" />
+                    <Field className="input" type="number" name="meta_dia_s1" />
+                    <ErrorMessage name="meta_dia_s1"/>
                 </div>
                 <div className="form-Group">
                     <label className="label">meta_dia_s2:</label>
-                    <Field className="input" type="number" name="" />
+                    <Field className="input" type="number" name="meta_dia_s2" />
+                    <ErrorMessage name="meta_dia_s2"/>
                 </div>
                 <div className="form-Group">
                     <label className="label">meta_dia_s3:</label>
-                    <Field className="input" type="number" name="" />
+                    <Field className="input" type="number" name="meta_dia_s3" />
+                    <ErrorMessage name="meta_dia_s3"/>
                 </div>
                 <div className="form-Group">
                     <label className="label">meta_dia_s4:</label>
-                    <Field className="input" type="number" name="" />
+                    <Field className="input" type="number" name="meta_dia_s4" />
+                    <ErrorMessage name="meta_dia_s4"/>
+
                 </div>
                 <div className="form-Group">
                     <label className="label">meta_dia_s5:</label>
-                    <Field className="input" type="number" name="" />
+                    <Field className="input" type="number" name="meta_dia_s5" />
+                    <ErrorMessage name="meta_dia_s5"/>
                 </div>   
             </div>
+
             <Button>Enviar</Button>
           </div>
           
--- src/components/TableMetaAsesor.jsx
+++ src/components/TableMetaAsesor.jsx
@@ -1,69 +1,64 @@
-import React from 'react';
-import './TableMetaAsesor.css'
+import React, { useState, useEffect } from "react";
+import "./TableMetaAsesor.css";
+import axios from "axios";
 
-const TableMetaAsesor= ()=>{
+const TableMetaAsesor = () => {
+  const [dataTable, setDataTable] = useState([]);
 
-    return(
-    
-        
-        <div className="container-table">
-        <div className="titel-table">
-            Metas Asesor
-        </div>
+  useEffect(() => {
+    axios
+      .get(`https://api.amstigo.com.co/mto/mestasAsesor/index.php`)
+      .then(function (res) {
+        setDataTable(res.data);
+      });
+  }, []);
 
-        <div className="data-table">
+  return (
+    <>
+      <div className="misTables">
+      {dataTable.map((x) => (
+        <div key={x.id} className="container-table">
+          <div className="titel-table">Metas Asesor</div>
+
+          <div className="data-table">
             <div className="data-table_grid">
-                <div className="grid-column light-gray line-button">Antiguedad</div>
-                <div className="grid-column light-gray line-button">KPI</div>
-                <div className="grid-column line-button">Acum. Mes</div>
-                <div className="grid-column line-button">S1</div>
-                <div className="grid-column line-button">S2</div>
-                <div className="grid-column line-button">S3</div>
-                <div className="grid-column line-button">S4</div>
-                <div className="grid-column line-button">S5</div>
-            
-                <div className="grid-row light-gray bold-font text-button">Mayor 60 </div>
-                <div className="grid-row light-gray">Bitacoras</div>
-                <div className="grid-row">58</div>
-                <div className="number-data">0</div>
-                <div className="number-data">0</div>
-                <div className="number-data">11</div>
-                <div className="number-data">12</div>
-                <div className="number-data">12</div>
+              <div className="grid-column light-gray line-button">
+                Antiguedad
+              </div>
+              <div className="grid-column light-gray line-button">KPI</div>
+              <div className="grid-column line-button">Acum. Mes</div>
+              <div className="grid-column line-button">S1</div>
+              <div className="grid-column line-button">S2</div>
+              <div className="grid-column line-button">S3</div>
+              <div className="grid-column line-button">S4</div>
+              <div className="grid-column line-button">S5</div>
+              <div className="grid-row light-gray bold-font text-button">
+                {x.Antiguedad}{" "}
+              </div>
+              <div className="grid-row light-gray">Bitacoras</div>
+              <div className="grid-row">{x.Meta_Bitacoras}</div>
+              <div className="number-data">{x.Meta_Bitacoras_s1}</div>
+              <div className="number-data">{x.Meta_Bitacoras_s2}</div>
+              <div className="number-data">{x.Meta_Bitacoras_s3}</div>
+              <div className="number-data">{x.Meta_Bitacoras_s4}</div>
+              <div className="number-data">{x.Meta_Bitacoras_s5}</div>
 
-                <div className="grid-row light-gray line-button"></div>
-                <div className="grid-row light-gray line-button">Dias</div>
-                <div className="grid-row line-button">9</div>
-                <div className="number-data line-button">0</div>
-                <div className="number-data line-button">0</div>
-                <div className="number-data line-button">2</div>
-                <div className="number-data line-button">3</div>
-                <div className="number-data line-button">2</div>
-                
-                <div className="grid-row light-gray bold-font text-button">Menor 60</div>
-                <div className="grid-row light-gray">Bitacoras</div>
-                <div className="grid-row">45</div>
-                <div className="number-data">0</div>
-                <div className="number-data">0</div>
-                <div className="number-data">11</div>
-                <div className="number-data">12</div>
-                <div className="number-data">12</div>
-
-                <div className="grid-row light-gray"></div>
-                <div className="grid-row light-gray">Dias</div>
-                <div className="grid-row ">9</div>
-                <div className="number-data">0</div>
-                <div className="number-data">0</div>
-                <div className="number-data">4</div>
-                <div className="number-data">3</div>
-                <div className="number-data">2</div>           
-            </div>        
+              <div className="grid-row light-gray line-button"></div>
+              <div className="grid-row light-gray line-button">Dias</div>
+              <div className="grid-row line-button">{x.Meta_dia}</div>
+              <div className="number-data line-button">{x.Meta_dia_s1}</div>
+              <div className="number-data line-button">{x.Meta_dia_s2}</div>
+              <div className="number-data line-button">{x.Meta_dia_s3}</div>
+              <div className="number-data line-button">{x.Meta_dia_s4}</div>
+              <div className="number-data line-button">{x.Meta_dia_s5}</div>
+            </div>
+          </div>
         </div>
-    </div> 
-        
-        
-       
-    )
-}
+      ))}
+
+      </div>
+    </>
+  );
+};
 
 export default TableMetaAsesor;
